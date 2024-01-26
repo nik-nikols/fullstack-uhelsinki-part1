f3f1287952bdd1a403a350d1dd7bf15f51572297
Nikolay Yarunin <nik.yarunin.dev@gmail.com>
2024-01-25 19:08:02 -0500

    Exercise 1.11

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -8,7 +8,7 @@ const Button = ({ handleClick, text }) => {
 
 const StatisticLine = ({ text, value }) => {
   return (
-    <div>{text} {value}</div>
+    <tr><td>{text}</td><td>{value}</td></tr>
   )
 }
 
@@ -19,12 +19,14 @@ const Statistics = ({good, bad, neutral}) => {
     return (
       <>
       <h2>statistics</h2>
+      <table><tbody>
       <StatisticLine text="good" value={good} />
       <StatisticLine text="neutral" value={neutral} />
       <StatisticLine text="bad" value={bad} />
       <StatisticLine text="all" value={total} />
       <StatisticLine text="average" value={(good - bad)/total} />
       <StatisticLine text="positive" value={good/total*100+ '%'} />
+      </tbody></table>
       </>
     )
   }
